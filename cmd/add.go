4c5e623046f590e0baed879189aa7a1286f369d7
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-05-02 19:05:16 +0530

    add and list cli working

--- cmd/add.go
+++ cmd/add.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"strings"
 
+	"clido/db"
+
 	"github.com/spf13/cobra"
 )
 
@@ -11,6 +13,12 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add a task to the list",
 	Run: func(cmd *cobra.Command, args []string) {
+		task := strings.Join(args, " ")
+		_, err := db.CreateTask(task)
+		if err != nil {
+			fmt.Println("Some error occurred", err.Error(), "can't continue")
+			return
+		}
 		fmt.Println("Added", strings.Join(args, " "), "to clido list")
 	},
 }
--- cmd/list.go
+++ cmd/list.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+
+	"clido/db"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +13,20 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all of the tasks not done",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hellow")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Some error occurred", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks left, add one, maybe? ")
+			return
+		}
+		fmt.Println("Here are all the tasks still left:")
+		for idx, task := range tasks {
+			fmt.Printf("%d. %s\n", idx+1, string(task.Value))
+
+		}
 	},
 }
 
