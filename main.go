d9e4aa1acfd7f41efd62ed478768e350ce916db6
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-05-02 18:45:05 +0530

    db actions

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"clido/cmd"
+	"clido/db"
+
+	"github.com/mitchellh/go-homedir"
 )
 
 func main() {
-	if err := cmd.RootCmd.Execute(); err != nil {
-		fmt.Println(err)
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 }
