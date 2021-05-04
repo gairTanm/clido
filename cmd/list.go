4ff7e99415b6aed758ddf959720426dbb6a91d8e
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2021-05-05 04:12:27 +0530

    late flag

--- cmd/list.go
+++ cmd/list.go
@@ -17,6 +17,9 @@ var listCmd = &cobra.Command{
 	Short: "List all of the tasks not done",
 	Run: func(cmd *cobra.Command, args []string) {
 		tasks, err := db.AllTasks()
+		fmt.Println("--------------------------------------------------------------------------------------------------------")
+		lateFlag, _ := cmd.Flags().GetBool("late")
+
 		var onTime, late []db.Task
 		if err != nil {
 			fmt.Println(chalk.Red.Color("Some error occurred"), chalk.Red.Color(err.Error()))
@@ -36,9 +39,18 @@ var listCmd = &cobra.Command{
 				fmt.Printf("%s%d. %s: %s %d%s\n", chalk.Red, idx+1, task.Value, task.Start.Month(), task.Start.Day(), chalk.Reset)
 			}
 		}
+		if lateFlag {
+			fmt.Println("--------------------------------------------------------------------------------------------------------")
+			fmt.Printf("%sHere are all the tasks you're running late on!%s\n", chalk.Red, chalk.Reset)
+			for _, lateTask := range late {
+				fmt.Printf("%s- %s: %s %d%s\n", chalk.Red, lateTask.Value, lateTask.Start.Month(), lateTask.Start.Day(), chalk.Reset)
+			}
+		}
+		fmt.Println("--------------------------------------------------------------------------------------------------------")
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(listCmd)
+	listCmd.Flags().Bool("late", false, "See all the tasks which are running late")
 }
